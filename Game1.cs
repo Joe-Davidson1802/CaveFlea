06109e4202849e8f731f2440c7f60477fb048064
Joe-Davidson1802 <Joe.Davidson2111@hotmail.com>
2016-10-11 13:53:26 +0000

    Adding camera

--- Game1.cs
+++ Game1.cs
@@ -27,14 +27,16 @@ namespace CaveFlea
         private int JumpCount2 = 0;
         private SpriteFont pixelFont;
         private List<Keys> keysPressed = new List<Keys>();
+        private bool Multiplayer;
+        Camera2D _camera;
 
-        public Game1()
+        public Game1(bool multiplayer)
         {
-
+            Multiplayer = multiplayer;
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-
+        
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -54,6 +56,8 @@ namespace CaveFlea
             graphics.PreferredBackBufferWidth = map.MapWidth * 5;
             graphics.ApplyChanges();
 
+            _camera = new Camera2D(GraphicsDevice.Viewport);
+            _camera.Zoom = 5;
 
             Random r = new Random();
 
@@ -66,13 +70,16 @@ namespace CaveFlea
             }
             PlayerPos = new Vector2(x, y);
             map.Map[(int)PlayerPos.X, (int)PlayerPos.Y] = 2;
-            do
+            if (Multiplayer)
             {
-                x = r.Next(2, map.MapWidth - 2);
-            }
-            while (map.Map[x, y] != 0 || map.Map[x, y] == 2);
+                do
+                {
+                    x = r.Next(2, map.MapWidth - 2);
+                }
+                while (map.Map[x, y] != 0 || map.Map[x, y] == 2);
                 Player2Pos = new Vector2(x, y);
-            map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
+                map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
+            }
             base.Initialize();
         }
 
@@ -125,14 +132,16 @@ namespace CaveFlea
             }
             PlayerPos = new Vector2(x, y);
             map.Map[(int)PlayerPos.X, (int)PlayerPos.Y] = 2;
-
-            do
+            if (Multiplayer)
             {
-                x = r.Next(2, map.MapWidth - 2);
+                do
+                {
+                    x = r.Next(2, map.MapWidth - 2);
+                }
+                while (map.Map[x, y] != 0 || map.Map[x, y] == 2);
+                Player2Pos = new Vector2(x, y);
+                map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
             }
-            while (map.Map[x, y] != 0 || map.Map[x, y] == 2);
-            Player2Pos = new Vector2(x, y);
-            map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
         }
 
         /// <summary>
@@ -155,16 +164,17 @@ namespace CaveFlea
             {
                 keysPressed.Add(key);
             }
-            if (PlayerPos.Y < 30 || Player2Pos.Y < 30)
+            if (PlayerPos.Y < 30 || (Multiplayer && Player2Pos.Y < 30))
             {
                 if (PlayerPos.Y < 30)
                     p1Wins++;
-                if (Player2Pos.Y < 30)
+                if (Multiplayer && Player2Pos.Y < 30)
                     p2Wins++;
                 Regen();
             }
             map.Map[(int)PlayerPos.X, (int)PlayerPos.Y] = 0;
-            map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 0;
+            if(Multiplayer)
+                map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 0;
             if (JumpCount != 0)
             {
                 JumpCount -= 1;
@@ -182,7 +192,7 @@ namespace CaveFlea
                 PlayerPos.Y++;
             }
 
-            if (JumpCount2 != 0)
+            if (Multiplayer && JumpCount2 != 0)
             {
                 JumpCount2 -= 1;
                 if (map.Map[(int)Player2Pos.X, (int)Player2Pos.Y - 1] == 0)
@@ -194,7 +204,7 @@ namespace CaveFlea
                     JumpCount2 = 0;
                 }
             }
-            else if (Player2Pos.Y + 1 < map.MapHeight && map.Map[(int)Player2Pos.X, (int)Player2Pos.Y + 1] == 0 && Player2Pos.Y < map.MapHeight - 1)
+            else if (Multiplayer && Player2Pos.Y + 1 < map.MapHeight && map.Map[(int)Player2Pos.X, (int)Player2Pos.Y + 1] == 0 && Player2Pos.Y < map.MapHeight - 1)
             {
                 Player2Pos.Y++;
             }
@@ -208,7 +218,7 @@ namespace CaveFlea
             {
                 PlayerPos.X++;
             }
-            if ((Player2Pos.X + 1 < map.MapWidth && keysPressed.Contains(Keys.D) && map.Map[(int)Player2Pos.X + 1, (int)Player2Pos.Y] == 0))
+            if (Multiplayer && (Player2Pos.X + 1 < map.MapWidth && keysPressed.Contains(Keys.D) && map.Map[(int)Player2Pos.X + 1, (int)Player2Pos.Y] == 0))
             {
                 Player2Pos.X++;
             }
@@ -217,7 +227,7 @@ namespace CaveFlea
             {
                 PlayerPos.X--;
             }
-            if (Player2Pos.X - 1 >= 0 && keysPressed.Contains(Keys.A) && map.Map[(int)Player2Pos.X - 1, (int)Player2Pos.Y] == 0)
+            if (Multiplayer && Player2Pos.X - 1 >= 0 && keysPressed.Contains(Keys.A) && map.Map[(int)Player2Pos.X - 1, (int)Player2Pos.Y] == 0)
             {
                 Player2Pos.X--;
             }
@@ -230,7 +240,7 @@ namespace CaveFlea
                     JumpCount = 10;
                 }
             }
-            if ((keysPressed.Contains(Keys.W) || keysPressed.Contains(Keys.V)))
+            if (Multiplayer && (keysPressed.Contains(Keys.W) || keysPressed.Contains(Keys.V)))
             {
                 if (JumpCount2 == 0 && (Player2Pos.Y == map.MapHeight - 1 || map.Map[(int)Player2Pos.X, (int)Player2Pos.Y + 1] != 0))
                 {
@@ -242,7 +252,7 @@ namespace CaveFlea
             {
                 map.Map[(int)PlayerPos.X + 1, (int)PlayerPos.Y] = 0;
             }
-            if ((Player2Pos.X + 1 < map.MapWidth && (keysPressed.Contains(Keys.D) && keysPressed.Contains(Keys.Space))) && map.Map[(int)Player2Pos.X + 1, (int)Player2Pos.Y] == 1)
+            if (Multiplayer && (Player2Pos.X + 1 < map.MapWidth && (keysPressed.Contains(Keys.D) && keysPressed.Contains(Keys.Space))) && map.Map[(int)Player2Pos.X + 1, (int)Player2Pos.Y] == 1)
             {
                 map.Map[(int)Player2Pos.X + 1, (int)Player2Pos.Y] = 0;
             }
@@ -251,7 +261,7 @@ namespace CaveFlea
             {
                 map.Map[(int)PlayerPos.X - 1, (int)PlayerPos.Y] = 0;
             }
-            if (Player2Pos.X - 1 >= 0 && (keysPressed.Contains(Keys.A) && keysPressed.Contains(Keys.Space)) && map.Map[(int)Player2Pos.X - 1, (int)Player2Pos.Y] == 1)
+            if (Multiplayer && Player2Pos.X - 1 >= 0 && (keysPressed.Contains(Keys.A) && keysPressed.Contains(Keys.Space)) && map.Map[(int)Player2Pos.X - 1, (int)Player2Pos.Y] == 1)
             {
                 map.Map[(int)Player2Pos.X - 1, (int)Player2Pos.Y] = 0;
             }
@@ -261,15 +271,25 @@ namespace CaveFlea
             {
                 map.Map[(int)PlayerPos.X, (int)PlayerPos.Y + 1] = 0;
             }
-            if (Player2Pos.Y + 1 < map.MapHeight && (keysPressed.Contains(Keys.S) && keysPressed.Contains(Keys.Space))
+            if (Multiplayer && Player2Pos.Y + 1 < map.MapHeight && (keysPressed.Contains(Keys.S) && keysPressed.Contains(Keys.Space))
                 && map.Map[(int)Player2Pos.X, (int)Player2Pos.Y + 1] == 1)
             {
                 map.Map[(int)Player2Pos.X, (int)Player2Pos.Y + 1] = 0;
             }
 
             map.Map[(int)PlayerPos.X, (int)PlayerPos.Y] = 2;
-            map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
+            if(Multiplayer)
+                map.Map[(int)Player2Pos.X, (int)Player2Pos.Y] = 3;
             // TODO: Add your update logic here
+            float posx = GraphicsDevice.Viewport.Width / 2;
+            float posy = GraphicsDevice.Viewport.Height / 2;
+            float temp = posx + ((GraphicsDevice.Viewport.Width / 5)/2);
+            if (posx + ((GraphicsDevice.Viewport.Width / 5) / 2) > GraphicsDevice.Viewport.Width)
+            {
+                posx = GraphicsDevice.Viewport.Width - (GraphicsDevice.Viewport.Width / 5);
+            }
+
+            _camera.Position = (PlayerPos * 5) - (new Vector2(posx, posy));
 
             base.Update(gameTime);
         }
@@ -283,7 +303,8 @@ namespace CaveFlea
             GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin();
+            var viewMatrix = _camera.GetViewMatrix();
+            spriteBatch.Begin(transformMatrix: (!Multiplayer) ? viewMatrix : Microsoft.Xna.Framework.Matrix.Identity);
 
             for (int i = 0; i < map.MapWidth; i++)
             {
@@ -291,7 +312,7 @@ namespace CaveFlea
                 {
                     Vector2 position = new Vector2(i * 5, x * 5);
                     Color col = x < 20 ? Color.LimeGreen : Color.White;
-                    if (Math.Abs(i - PlayerPos.X) + Math.Abs(x - PlayerPos.Y) < 30 || Math.Abs(i - Player2Pos.X) + Math.Abs(x - Player2Pos.Y) < 30 || x < 30)
+                    if (Math.Abs(i - PlayerPos.X) + Math.Abs(x - PlayerPos.Y) < 30 || (Multiplayer && Math.Abs(i - Player2Pos.X) + Math.Abs(x - Player2Pos.Y) < 30 || x < 30))
                     {
                         int grades = 0;
                         float grading = 0;
@@ -307,7 +328,7 @@ namespace CaveFlea
                             grading = (255 / 30) * (Math.Abs(i - PlayerPos.X) + Math.Abs(x - PlayerPos.Y));
                             grades++;
                         }
-                        if (Math.Abs(i - Player2Pos.X) + Math.Abs(x - Player2Pos.Y) < 30)
+                        if (Multiplayer && Math.Abs(i - Player2Pos.X) + Math.Abs(x - Player2Pos.Y) < 30)
                         {
                             grading2 = (255 / 30) * (Math.Abs(i - Player2Pos.X) + Math.Abs(x - Player2Pos.Y));
                             grades++;
@@ -367,17 +388,16 @@ namespace CaveFlea
                     }
                 }
             }
-            Vector2 stringl1 = pixelFont.MeasureString("PLAYER 1 WINS 0 | PLAYER 2 WINS 0");
+            Vector2 stringl1 = pixelFont.MeasureString((Multiplayer) ? "PLAYER 1 WINS 0 | PLAYER 2 WINS 0": "WIN COUNT 0");
             Vector2 stringl2 = pixelFont.MeasureString("GET HERE TO WIN");
 
             float paddingLeft = (GraphicsDevice.DisplayMode.Width / 2) - (stringl1.X / 2);
             float paddingLeft2 = (GraphicsDevice.DisplayMode.Width / 2) - (stringl2.X / 2);
 
-            spriteBatch.DrawString(pixelFont, string.Format("PLAYER 1 WINS {0} | PLAYER 2 WINS {1}", p2Wins, p1Wins), new Vector2(paddingLeft + 5, 15), new Color(Color.Black, 253));
-            spriteBatch.DrawString(pixelFont, string.Format("PLAYER 1 WINS {0} | PLAYER 2 WINS {1}", p2Wins, p1Wins), new Vector2(paddingLeft, 10), new Color(Color.White, 253));
+            spriteBatch.DrawString(pixelFont, (Multiplayer) ? string.Format("PLAYER 1 WINS {0} | PLAYER 2 WINS {1}", p2Wins, p1Wins): string.Format("WIN COUNT {0}", p1Wins), new Vector2(paddingLeft + 5, 15), new Color(Color.Black, 253));
+            spriteBatch.DrawString(pixelFont, (Multiplayer) ? string.Format("PLAYER 1 WINS {0} | PLAYER 2 WINS {1}", p2Wins, p1Wins) : string.Format("WIN COUNT {0}", p1Wins), new Vector2(paddingLeft, 10), new Color(Color.White, 253));
             spriteBatch.DrawString(pixelFont, string.Format("GET HERE TO WIN"), new Vector2(paddingLeft2 + 5, 75), new Color(Color.Black, 253));
             spriteBatch.DrawString(pixelFont, string.Format("GET HERE TO WIN"), new Vector2(paddingLeft2, 70), new Color(Color.White, 253));
-
 
             spriteBatch.End();
 
@@ -607,6 +627,35 @@ namespace CaveFlea
             this.PercentAreWalls = percentWalls;
             this.Map = new int[this.MapWidth, this.MapHeight];
             this.Map = map;
+        }
+    }
+    public class Camera2D
+    {
+        private readonly Viewport _viewport;
+
+        public Camera2D(Viewport viewport)
+        {
+            _viewport = viewport;
+
+            Rotation = 0;
+            Zoom = 1;
+            Origin = new Vector2(viewport.Width / 2f, viewport.Height / 2f);
+            Position = Vector2.Zero;
+        }
+
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public float Zoom { get; set; }
+        public Vector2 Origin { get; set; }
+
+        public Matrix GetViewMatrix()
+        {
+            return
+                Matrix.CreateTranslation(new Vector3(-Position, 0.0f)) *
+                Matrix.CreateTranslation(new Vector3(-Origin, 0.0f)) *
+                Matrix.CreateRotationZ(Rotation) *
+                Matrix.CreateScale(Zoom, Zoom, 1) *
+                Matrix.CreateTranslation(new Vector3(Origin, 0.0f));
         }
     }
 }
